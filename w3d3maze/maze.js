90ae67be1f8d2124ece6d9a56c659bc6053a8532
Rachel Aitaru <aitarurachel@gmail.com>
2020-07-08 20:40:07 -0500

    added assignment files

---
+++ w3d3maze/maze.js
@@ -0,0 +1,27 @@
+'use strict';
+
+$(document).ready(()=>{
+
+    let looser = false;
+
+    $('div.boundary').on('mouseenter',()=>{
+        $('div.boundary').addClass('youlose');
+        $('h2#status').html('You lose');
+        looser = true;
+    })
+
+    $('div#end').on('mouseenter',()=>{
+        if(looser === false){
+            $('h2#status').html('You win! :]');
+        }
+        else{
+            $('h2#status').html('You lose');
+        }
+    })
+
+    $('div#start').on('mouseenter',()=>{
+        $('div.boundary').removeClass('youlose');
+        $('h2#status').html('Click the "S" to begin');
+        looser = false;
+    })
+});
